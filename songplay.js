ba488af195789282af10ca8ecb322d28d77d4e05
OoooooM.999 <64514042+omthapa779@users.noreply.github.com>
2022-05-21 11:21:11 +0545

    Add files via upload

---
+++ songplay.js
@@ -0,0 +1,126 @@
+function play(){
+var elem = document.getElementById("pewds_play");
+    if (elem.value=="PLAY !"){
+        elem.value = "Restart";
+        document.getElementById("audio_controls").pause();
+document.getElementById("audio_controls").setAttribute('src', './audio/pew die pie.mp3');
+document.getElementById("audio_controls").load();
+document.getElementById("audio_controls").play();
+    }
+    else{
+        elem.value = "PLAY !";
+        document.getElementById("audio_controls").pause();
+    }
+}
+function play2(){
+    var elem = document.getElementById("tnt");
+    if (elem.value=="PLAY !"){
+        elem.value = "Restart";
+        document.getElementById("audio_controls").pause();
+document.getElementById("audio_controls").setAttribute('src', './audio/tnt.mp3');
+document.getElementById("audio_controls").load();
+document.getElementById("audio_controls").play();
+    }
+    else{
+        elem.value = "PLAY !";
+        document.getElementById("audio_controls").pause();
+    }
+}
+function play3(){
+    var elem = document.getElementById("feeling_good");
+    if (elem.value=="PLAY !"){
+        elem.value = "Restart";
+        document.getElementById("audio_controls").pause();
+document.getElementById("audio_controls").setAttribute('src', './audio/feeling_good.mp3');
+document.getElementById("audio_controls").load();
+document.getElementById("audio_controls").play();
+    }
+    else{
+        elem.value = "PLAY !";
+        document.getElementById("audio_controls").pause();
+    }
+}
+function play4(){
+    var elem = document.getElementById("look_at_me");
+    if (elem.value=="PLAY !"){
+        elem.value = "Restart";
+        document.getElementById("audio_controls").pause();
+document.getElementById("audio_controls").setAttribute('src', './audio/look at me.mp3');
+document.getElementById("audio_controls").load();
+document.getElementById("audio_controls").play();
+    }
+    else{
+        elem.value = "PLAY !";
+        document.getElementById("audio_controls").pause();
+    }
+}
+function play5(){
+    var elem = document.getElementById("kagbeni");
+    if (elem.value=="PLAY !"){
+        elem.value = "Restart";
+        document.getElementById("audio_controls").pause();
+document.getElementById("audio_controls").setAttribute('src', './audio/kagbeni.mp3');
+document.getElementById("audio_controls").load();
+document.getElementById("audio_controls").play();
+    }
+    else{
+        elem.value = "PLAY !";
+        document.getElementById("audio_controls").pause();
+    }
+}
+function play6(){
+    var elem = document.getElementById("tuyo");
+    if (elem.value=="PLAY !"){
+        elem.value = "Restart";
+        document.getElementById("audio_controls").pause();
+document.getElementById("audio_controls").setAttribute('src', './audio/tuyo.mp3');
+document.getElementById("audio_controls").load();
+document.getElementById("audio_controls").play();
+    }
+    else{
+        elem.value = "PLAY !";
+        document.getElementById("audio_controls").pause();
+    }
+}
+function play7(){
+    var elem = document.getElementById("mercury");
+    if (elem.value=="PLAY !"){
+        elem.value = "Restart";
+        document.getElementById("audio_controls").pause();
+document.getElementById("audio_controls").setAttribute('src', './audio/mercury.mp3');
+document.getElementById("audio_controls").load();
+document.getElementById("audio_controls").play();
+    }
+    else{
+        elem.value = "PLAY !";
+        document.getElementById("audio_controls").pause();
+    }
+}
+function play8(){
+    var elem = document.getElementById("1994");
+    if (elem.value=="PLAY !"){
+        elem.value = "Restart";
+        document.getElementById("audio_controls").pause();
+document.getElementById("audio_controls").setAttribute('src', './audio/1994.mp3');
+document.getElementById("audio_controls").load();
+document.getElementById("audio_controls").play();
+    }
+    else{
+        elem.value = "PLAY !";
+        document.getElementById("audio_controls").pause();
+    }
+}
+function play9(){
+    var elem = document.getElementById("robbery");
+    if (elem.value=="PLAY !"){
+        elem.value = "Restart";
+        document.getElementById("audio_controls").pause();
+document.getElementById("audio_controls").setAttribute('src', './audio/robbery.mp3');
+document.getElementById("audio_controls").load();
+document.getElementById("audio_controls").play();
+    }
+    else{
+        elem.value = "PLAY !";
+        document.getElementById("audio_controls").pause();
+    }
+}
